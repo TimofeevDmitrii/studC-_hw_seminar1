86868505f1d21e5bf19176013902d7dddca14a71
Dmitry <dimatimoha2014@gmail.com>
2023-09-20 12:43:07 +0300

    HW seminar9 was added

---
+++ stud_Csharp_hw_seminar9/task64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string PrintNumsFromNTo1(int n)
+{
+    if (n==1)
+        return $"{n}";
+    else
+        return $"{n}, "+PrintNumsFromNTo1(n-1);
+}
+
+System.Console.WriteLine("Введите натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(PrintNumsFromNTo1(n));
+
+
---
+++ stud_Csharp_hw_seminar9/task66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int FindSumFromMintoMax(int min, int max)
+{
+    if (min==max)
+        return max;
+    else
+        return min+FindSumFromMintoMax(min+1,max);
+}
+
+System.Console.WriteLine("Введите натуральное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите натуральное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {FindSumFromMintoMax(m,n)}");
